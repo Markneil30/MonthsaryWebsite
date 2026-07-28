8541aab5094078b86242f26de8f2d75408f3898c
neilmark <neilmark@gmail.com>
2026-07-27 18:26:51 -0700

    Initial commit

---
+++ gallery.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<meta charset="UTF-8">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Our Memories 💜</title>
+
+<link rel="stylesheet" href="style.css">
+
+</head>
+
+
+<body class="galleryPage">
+
+<a href="index.php" class="back-btn">
+    ← Back
+</a>
+
+<h1>Our Memories 💜</h1>
+
+
+<div class="single-photo">
+
+<img src="images/pic1.jpg">
+
+</div>
+
+
+
+<!-- SCROLL INDICATOR -->
+
+<div class="scroll-down">
+
+    <span>Scroll Down</span>
+
+    <div class="arrow">↓</div>
+
+</div>
+
+
+
+
+<!-- MESSAGE PAPER -->
+
+
+<div class="letter">
+
+
+<h2>My Message 💜</h2>
+
+
+<p>
+
+Happy Monthsary love 💜
+
+<br><br>
+
+I just want to say thank you for everything.
+Thank you for staying with me and for all the memories
+that we created together.
+
+<br><br>
+
+I hope we continue making more beautiful moments
+together. I love you always. 💜
+
+</p>
+
+
+</div>
+
+
+
+</body>
+
+</html>
